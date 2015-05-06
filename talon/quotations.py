14b7e6ae36ff7a0bd6a923c27b23fb67e21cc48a
Simon <simon.flore.hf@gmail.com>
2015-05-06 11:51:49 +0200

    NL support

--- talon/quotations.py
+++ talon/quotations.py
@@ -56,7 +56,7 @@ RE_ON_DATE_SMB_WROTE = re.compile(
     ))
 # Special case for languages where text is translated like this: 'on {date} wrote {somebody}:'
 RE_ON_DATE_WROTE_SMB = re.compile(
-    u'(-*[ ]?({0})[ ].*(.*\n){{0,2}}.*({1})[ ].*:)'.format(
+    u'(-*[ ]?({0})[ ].*(.*\n){{0,2}}.*({1})[ ]*.*:)'.format(
         # Beginning of the line
         	'Op',
         # Ending of the line
