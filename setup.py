31803d41bcabb3266e9b8ad7c18ba84f9d5cf61b
Sergey Obukhov <sergey.obykhov@mailgunhq.com>
2016-02-19 18:30:43 -0800

    fixes mailgun/talon#18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='talon',
-      version='1.2.2',
+      version='1.2.3',
       description=("Mailgun library "
                    "to extract message quotations and signatures."),
       long_description=open("README.rst").read(),
