2ecd9779fc9883168a787702f724c443ec30f806
Sergey Obukhov <sergey.obykhov@mailgunhq.com>
2016-02-19 18:32:07 -0800

    bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='talon',
-      version='1.2.1',
+      version='1.2.2',
       description=("Mailgun library "
                    "to extract message quotations and signatures."),
       long_description=open("README.rst").read(),
