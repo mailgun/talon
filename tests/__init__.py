243be9e92e5b992c73f17ff6e146a1f4eee1c99b
Anders Kaseorg <andersk@mit.edu>
2022-07-29 18:48:39 -0700

    Find test fixture files relative to __file__
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,18 +1,20 @@
 from __future__ import absolute_import
+import os
 from nose.tools import *
 from mock import *
 
 import talon
 
 
-EML_MSG_FILENAME = "tests/fixtures/standard_replies/yahoo.eml"
-MSG_FILENAME_WITH_BODY_SUFFIX = ("tests/fixtures/signature/emails/P/"
+FIXTURES_DIR = os.path.dirname(__file__) + "/fixtures"
+EML_MSG_FILENAME = FIXTURES_DIR + "/standard_replies/yahoo.eml"
+MSG_FILENAME_WITH_BODY_SUFFIX = (FIXTURES_DIR + "/signature/emails/P/"
                                  "johndoeexamplecom_body")
-EMAILS_DIR = "tests/fixtures/signature/emails"
-TMP_DIR = "tests/fixtures/signature/tmp"
+EMAILS_DIR = FIXTURES_DIR + "/signature/emails"
+TMP_DIR = FIXTURES_DIR + "/signature/tmp"
 
-STRIPPED = "tests/fixtures/signature/emails/stripped/"
-UNICODE_MSG = ("tests/fixtures/signature/emails/P/"
+STRIPPED = FIXTURES_DIR + "/signature/emails/stripped/"
+UNICODE_MSG = (FIXTURES_DIR + "/signature/emails/P/"
                "unicode_msg")
 
 
