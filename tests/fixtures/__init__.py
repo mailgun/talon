f05f274cad5f22b08313520bd6e1180c0acbc3a1
Anders Kaseorg <andersk@mit.edu>
2022-07-29 18:48:39 -0700

    Find test fixture files relative to __file__
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -1,10 +1,13 @@
-STANDARD_REPLIES = "tests/fixtures/standard_replies"
+import os
 
-with open("tests/fixtures/reply-quotations-share-block.eml") as f:
+FIXTURES_DIR = os.path.dirname(__file__)
+STANDARD_REPLIES = FIXTURES_DIR + "/standard_replies"
+
+with open(FIXTURES_DIR + "/reply-quotations-share-block.eml") as f:
     REPLY_QUOTATIONS_SHARE_BLOCK = f.read()
 
-with open("tests/fixtures/OLK_SRC_BODY_SECTION.html") as f:
+with open(FIXTURES_DIR + "/OLK_SRC_BODY_SECTION.html") as f:
     OLK_SRC_BODY_SECTION = f.read()
 
-with open("tests/fixtures/reply-separated-by-hr.html") as f:
+with open(FIXTURES_DIR + "/reply-separated-by-hr.html") as f:
     REPLY_SEPARATED_BY_HR = f.read()
